14d1bd76e943193662e28c7af8e1dea13f182a89
nielsvelst <nielsvelst@hotmail.com>
2023-04-09 13:04:31 +0200

    huiswerk af

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,32 @@ public class Main {
     public static void main(String[] args) {
 
         // Hier mag je je code scrijven voor de hoofd-opdracht
+        Integer[] numeric = {1,2,3,4,5,6,7,8,9,0};
+        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen","nul"};
 
+        Translator translator = new Translator(alphabetic, numeric);
+        boolean play = true;
+        String ongeldig = "ongeldige invoer";
+        Scanner scanner = new Scanner(System.in);
+        while (play){
+            System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("x") ) {
+                play = false;
+            } else if(input.equalsIgnoreCase("v")) {
+                System.out.println("Type een cijfer in van 0 t/m 9");
+                int number = scanner.nextInt();
+                scanner.nextLine();
+                if(number < 10) {
+                    String result = translator.translate(number);
+                    System.out.println("De vertaling van " + number + " is " + result);
+                }else {
+                    System.out.println(ongeldig);
+                }
+            } else {
+                System.out.println(ongeldig);
+            }
+        }
         /* deze regel mag je weg halen voor de bonus opdracht. Onderstaande code is voor de bonus opdracht.
         HashSet<Integer> secretnumber = randomnumbergenerator();
         String stringnumber =  setToStringConverter(secretnumber);
